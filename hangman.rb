33a91396e30667720cc03d6fdfcd1065e0c2968a
Raymundo Alva <rayalva407@gmail.com>
2024-05-06 22:08:38 -0600

    Completes Hangman class functionality

--- hangman.rb
+++ hangman.rb
@@ -2,6 +2,8 @@ class Hangman
 
   def initialize
     @word = File.read("google-10000-english-no-swears.txt").split("\n").sample().downcase
+    @word_state = Array.new(@word.length, "_")
+    @lives = 6
   end
 
   def welcome
@@ -14,16 +16,40 @@ class Hangman
   end
 
   def display_word
-    @word.each_char do |char|
-      print "_ "
-    end
-    puts
+    puts @word_state.join(" ")
   end
-  
+
+  def display_lives
+    puts "You have #{@lives} lives left."
+  end
+
+  def update_word_state(guess)
+    @word.split("").each_with_index do |letter, index|
+      if letter == guess
+        @word_state[index] = guess
+      end
+    end
+  end
+
   def play
     welcome
-    @word
+    while @lives > 0
+      display_word
+      display_lives
+
+      guess = make_guess
+
+      if @word.include?(guess)
+        update_word_state(guess)
+      else
+        puts "Sorry, wrong guess! Try again."
+        @lives -= 1
+      end
+
+      if @word_state.join("") == @word
+        puts "Congratulations! You won!"
+        break
+      end
+    end
   end
-
-
 end
